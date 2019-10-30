ff9f23678e50b4a06ef3b9c01a8a9f6170856a16
Richard Li <aspirewit@gmail.com>
2019-10-30 16:21:35 +0800

    Update nvm-auto-switch.plugin.zsh

--- nvm-auto-switch.plugin.zsh
+++ nvm-auto-switch.plugin.zsh
@@ -1,10 +1,17 @@
+if [ "$(command -v nvm)" = "" ]; then
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+  else
+    echo "could not find nvm"
+  fi
+fi
+
 function nvm_auto_switch {
   local NVM_VERSION
   local NVM_RC_FILE=`nvm_find_nvmrc`
 
   if [ "$NVM_RC_FILE" = "" ]; then
-    local DEFAULT_VERSION=`nvm_alias default`
-    NVM_VERSION=`nvm_version $DEFAULT_VERSION`
+    NVM_VERSION=`nvm_version $(nvm_alias default)`
   else
     NVM_VERSION=`nvm_version $(cat $NVM_RC_FILE)`
   fi
